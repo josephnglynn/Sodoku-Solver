3e4a9e8d3cbb644f71a52e84759c856813b19a83
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-03-18 18:15:09 +0000

    MORE FIXES

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
-import serve from 'rollup-plugin-serve';
+import serve from "rollup-plugin-serve";
 import * as path from "path";
 
 
@@ -15,12 +15,12 @@ const production = !process.env.ROLLUP_WATCH;
 
 
 export default {
-	input: 'src/main.ts',
+	input: "src/main.ts",
 	output: {
 		sourcemap: !production,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		name: "app",
+		file: "public/build/bundle.js"
 	},
 	plugins: [
 		svelte({
@@ -30,18 +30,18 @@ export default {
 				dev: !production
 			}
 		}),
-		// we'll extract any component CSS out into
+		// we"ll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: "bundle.css" }),
 
 		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
+		// npm, you"ll most likely need these plugins. In
+		// some cases you"ll need additional configuration -
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ["svelte"]
 		}),
 		commonjs(),
 		typescript({
@@ -60,9 +60,9 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload("public"),
 
-		// If we're building for production (npm run build
+		// If we"re building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser()
 	],
