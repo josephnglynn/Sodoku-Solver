8a2531556777f057edc50a2a0c6808cd225a1f99
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-04-06 15:53:40 +0100

    Added More To Implementation

--- src/api/solveSudoku.ts
+++ src/api/solveSudoku.ts
@@ -1,5 +1,5 @@
 function isValid(state: Array<Array<number>>, row: number, col: number, k: number, n: number) {
-    for (let i = 0; i < n*n; i++) {
+    for (let i = 0; i < n * n; i++) {
         const q = n * Math.floor(row / n) + Math.floor(i / n);
         const p = n * Math.floor(col / n) + i % n;
         if (state[row][i] == k || state[i][col] == k || state[q][p] == k) {
@@ -14,7 +14,7 @@ const bruteForce = (state: Array<Array<number>>, n: number): boolean => {
     for (let i = 0; i < state.length; i++) {
         for (let j = 0; j < state.length; j++) {
             if (state[i][j] == 0) {
-                for (let k = 1; k <= n*n; k++) {
+                for (let k = 1; k <= n * n; k++) {
                     if (isValid(state, i, j, k, n)) {
                         state[i][j] = k;
                         if (bruteForce(state, n)) {
@@ -32,7 +32,7 @@ const bruteForce = (state: Array<Array<number>>, n: number): boolean => {
 }
 
 
-const checkSudoku = (state: Array<Array<number>>):boolean => {
+const checkSudoku = (state: Array<Array<number>>): boolean => {
     for (let row = 0; row < state.length; row++) {
         for (let col = 0; col < state.length; col++) {
             if (state[row][col] == 0) {
@@ -43,15 +43,15 @@ const checkSudoku = (state: Array<Array<number>>):boolean => {
     return true;
 }
 
-const bruteForceWrapper = (state: Array<Array<number>>, n: number):Array<Array<number>> => {
+const bruteForceWrapper = (state: Array<Array<number>>, n: number): Array<Array<number>> => {
     bruteForce(state, n)
     return state;
 }
 
 
-export const solvePartOfSudoku = (state: Array<Array<number>>, n: number, setState: (s: Array<Array<number>>) => void, showMessage: (message: String, onAccept: Function, onCancel: Function) => void ): [Array<Array<number>>, boolean] => { //need to return array for state change
+export const solvePartOfSudoku = (state: Array<Array<number>>, n: number, setState: (s: Array<Array<number>>) => void, showMessage: (message: String, onAccept: Function, onCancel: Function) => void): [Array<Array<number>>, boolean] => { //need to return array for state change
 
-    let average: number = calculateAverage(n*n);
+    let average: number = calculateAverage(n * n);
     if (checkRowsForOneLeft(state, average)) {
         return;
     }
@@ -69,20 +69,20 @@ export const solvePartOfSudoku = (state: Array<Array<number>>, n: number, setSta
     }
 
     //No Strategy Worked
-    if (showMessage == null){
+    if (showMessage == null) {
         bruteForceWrapper(state, n)
     } else {
-        showMessage("This Sudoku Proved To Be To Difficult\nReverting To Brute Force", ()=>setState(bruteForceWrapper(state, n)), ()=>{});
+        showMessage("This Sudoku Proved To Be To Difficult\nReverting To Brute Force", () => setState(bruteForceWrapper(state, n)), () => {
+        });
     }
 
     return [state, checkSudoku(state)];
 }
 
 
-
 const calculateAverage = (length: number) => {
     let count: number = 0;
-    for (let i = 1; i <=length; i++) {
+    for (let i = 1; i <= length; i++) {
         count += i;
     }
     return count;
@@ -110,7 +110,7 @@ const checkRowsForOneLeft = (state: Array<Array<number>>, average: number): bool
 }
 
 
-const checkColumnsForOneLeft  = (state: Array<Array<number>>, average: number): Boolean => { //CHECKS TO SEE HOW MANY NUMBERS ARE LEFT IN A COLUMN - IF ONLY ONE THEN CAN FILL IN
+const checkColumnsForOneLeft = (state: Array<Array<number>>, average: number): Boolean => { //CHECKS TO SEE HOW MANY NUMBERS ARE LEFT IN A COLUMN - IF ONLY ONE THEN CAN FILL IN
     // noinspection DuplicatedCode
     for (let i = 0; i < state.length; i++) {
         let count = 0;
@@ -132,16 +132,15 @@ const checkColumnsForOneLeft  = (state: Array<Array<number>>, average: number): 
 }
 
 
-
 const checkSquares = (state: Array<Array<number>>, n: number, average: number): Boolean => {
 
-    
+
     for (let i = 0; i < n; i++) {
         let totalNumberOfZeros = 0;
         let current = 0;
-        for (let k = (i*n); k < (i+1)*n; k++) {
-            for (let j = (i*n); j < (i+1)*n; j++) {
-                if(state[j][k] == 0) {
+        for (let k = (i * n); k < (i + 1) * n; k++) {
+            for (let j = (i * n); j < (i + 1) * n; j++) {
+                if (state[j][k] == 0) {
                     totalNumberOfZeros++;
                 } else {
                     current += state[j][k];
@@ -149,56 +148,101 @@ const checkSquares = (state: Array<Array<number>>, n: number, average: number): 
             }
         }
         if (totalNumberOfZeros == 1) {
-            for (let k = (i*n); k < (i+1)*n; k++) {
-                for (let j = (i*n); j < (i+1)*n; j++) {
-                    if(state[j][k] == 0) {
+            for (let k = (i * n); k < (i + 1) * n; k++) {
+                for (let j = (i * n); j < (i + 1) * n; j++) {
+                    if (state[j][k] == 0) {
                         state[j][k] = average - current;
                         return true;
-                    } 
+                    }
                 }
             }
         }
-        
+
     }
 
     return false;
 }
 
 
-
-
-
 const checkLargerRows = (state: Array<Array<number>>, n: number): Boolean => {
-    //check each number through all rows
-    
-    for (let i = 0; i < n; i++) {
-        /*
-        CHECKS ALL BIG COLUMNS - E.G:
-        [1 2] [3 4]
-        [3 4] [1 2]
-        [2 1] [4 3]
-        [4 3] [2 1]
-        IT WOULD CHECK THE COLUMN:
-        [1 2]
-        [3 4]
-        [2 1]
-        [4 3]
-         */
-        for (let k = (i*n); k < (i+1)*n; k++) {
-            /*
-            (i*n) so we move to correct column
-            IN EXAMPLE ABOVE, ON SECOND ITERATION - (i*n) = (1*2) = 2
-            SO WE START ITERATING BETWEEN THE VALUES 2 AND 3 IN THE EXAMPLE ABOVE ( INDEXING STARTING AT 0 )
-            K IS THE COLUMN NUMBER
-             */
-            for (let j = (i*n); j < (i+1)*n; j++) {
-                    /*
-                    HERE LIKE ABOVE WE DO (i*n)
-                    THIS TIME j IS THE ROW NUMBER
-                     */
-                    console.log(`I: ${i}, K: ${k}, J: ${j}`);
+    //Possible Values Of Numbers
+    let speculativeState: Array<Array<Array<number>>> = new Array<Array<Array<number>>>(n*n);
+    for (let i = 0; i < state.length; i++) {
+        speculativeState[i] = new Array<Array<number>>(n*n);
+        for (let k = 0; k < state.length; k++) {
+            speculativeState[i][k] = new Array<number>();
+        }
+    }
+
+    let totalPossibility: Array<number> = [];
+
+    for (let i = 1; i <= n * n; i++) {
+        totalPossibility.push(i);
+    }
+
+    //Fill up speculativeState with Numbers We know for sure And Fill Up Others With any possible value, correct or not
+    for (let i = 0; i < state.length; i++) { //Row
+        let unUsedNumbers: Array<number> = totalPossibility;
+        for (let k = 0; k < state.length; k++) { //Column
+            if (state[i][k] != 0) {
+                speculativeState[i][k] = [state[i][k]]; //Now We Know Squares With Known Numbers Have Length Of 1
+                unUsedNumbers.filter(((value, index, array) => value != state[i][k]));
+            }
+        }
+        for (let k = 0; k < state.length; k++) {
+            if (state[i][k] == 0) {
+                speculativeState[i][k] = unUsedNumbers; //We Fill It Up With Numbers Which We Know Are Possibilities
             }
         }
     }
+
+    for (let k = 0; k < state.length; k++) { //Column
+        let unUsedNumbers: Array<number> = totalPossibility;
+        for (let i = 0; i < state.length; i++) { //Row
+            if (state[i][k] != 0) {
+                speculativeState[i][k] = [state[i][k]]; //Now We Know Squares With Known Numbers Have Length Of 1
+                unUsedNumbers.filter(((value, index, array) => value != state[i][k]));
+            }
+        }
+        for (let i = 0; i < state.length; i++) {
+            let newValue: Array<number> = [];
+            speculativeState[i][k].forEach(value => {
+                let inBoth: Boolean = false;
+                unUsedNumbers.forEach(value1 => {
+                    if (value == value1) {
+                        inBoth = true;
+                    }
+                });
+                if (inBoth) {
+                    newValue.push(value);
+                }
+            });
+            speculativeState[i][k] = newValue;
+        }
+    }
+
+    let areTheSame: Boolean = true;
+
+    let tempState: Array<Array<number>> = new Array<Array<number>>(n*n);
+    for (let i = 0; i < state.length; i++) { //Row
+        tempState[i] = new Array<number>(n*n)
+        for (let k = 0; k < state.length; k++) { //Column
+            if (speculativeState[i][k].length == 1) {
+                tempState[i][k] = speculativeState[i][k][0];
+                if (state[i][k] == 0) {
+                    areTheSame = false;
+                }
+            } else {
+                tempState[i][k] = 0;
+            }
+        }
+    }
+
+    if (!areTheSame) {
+        console.log("NOT THE SAME");
+        state = tempState;
+        return true;
+    }
+
     return false;
 }
