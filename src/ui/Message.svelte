12f33d8ffe48306936facbc28a730e703d77b5f7
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-04-14 11:13:49 +0100

    Fixed Error With Cancel Button

--- src/ui/Message.svelte
+++ src/ui/Message.svelte
@@ -10,7 +10,7 @@
     <div style="align-self: center; background-color: black; padding: 20px; border-radius: 10px; margin: 20px">
         <h1 style="text-align: center; padding: 20px;" class="is-text has-text-white">{text}</h1>
         <div style="display: flex; justify-content: space-around; flex-direction: row; min-width: 150px">
-            <button class="is-danger button" style="padding: 10px" on:click={()=>{onCancel; clearMessage();}}>CANCEL</button>
+            <button class="is-danger button" style="padding: 10px" on:click={()=>{onCancel(); clearMessage();}}>CANCEL</button>
             <button class="is-primary button" style="padding: 10px" on:click={()=>{onAccept(); clearMessage();}}>OK</button>
         </div>
     </div>
