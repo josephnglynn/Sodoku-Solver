7ec5d613fce4e0fbeaa96f4cfd9f438484857760
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-04-17 15:45:35 +0100

    Added Cache To serviceWorker.js

---
+++ public/serviceWorker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "Sudoku_Solver_cache_v1.0.1";
+const URLS_TO_CACHE = [
+    "/Sudoku-Solver/",
+    "/Sudoku-Solver/index.html",
+    "/Sudoku-Solver/global.css",
+    "/Sudoku-Solver/manifest.webmanifest",
+    "/Sudoku-Solver/serviceWorker.js",
+    "/Sudoku-Solver/favicon.png",
+    "/Sudoku-Solver/build/bundle.css",
+    "/Sudoku-Solver/build/bundle.js"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log("Opened Cache");
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+})
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+})
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+      caches.keys().then(function (cacheNames) {
+          return Promise.all(
+              cacheNames.map(function (cacheName) {
+                  if (cacheName !== CACHE_NAME) {
+                      console.log("DELETING CACHE");
+                      return caches.delete(cacheName)
+                  }
+              })
+          );
+      })
+    );
+})
