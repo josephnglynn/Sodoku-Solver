e3377b67b40cf572a86f311905a1d534ffdff712
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-04-17 15:58:37 +0100

    Fixed Issue With Text Not Being Centered

--- src/ui/Message.svelte
+++ src/ui/Message.svelte
@@ -9,9 +9,9 @@
 <div style="width: 100vw; height: 100vh; position:relative; display: flex; justify-content: center">
     <div class="holder" style="align-self: center; background-color: black; padding: 20px; border-radius: 10px; margin: 20px">
         <h1 style="text-align: center; padding: 20px;" >{text}</h1>
-        <div style="display: flex; justify-content: space-around; flex-direction: row; min-width: 150px">
-            <button class="is-danger button" style="padding: 10px" on:click={()=>{onCancel(); clearMessage();}}>CANCEL</button>
-            <button class="is-primary button" style="padding: 10px" on:click={()=>{onAccept(); clearMessage();}}>OK</button>
+        <div style="display: flex; justify-content: space-around; flex-direction: row;">
+            <button class="is-danger button" on:click={()=>{onCancel(); clearMessage();}}>CANCEL</button>
+            <button class="is-primary button" on:click={()=>{onAccept(); clearMessage();}}>OK</button>
         </div>
     </div>
 </div>
